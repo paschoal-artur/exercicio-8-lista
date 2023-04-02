d0b839b242c4de9613221f2bf58336acb5873805
paschoal-artur <paschoal.artur@alu.ufc.br>
2023-04-01 23:44:52 -0300

    added code

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+#Receiving value from user
+money_hour = float(input("How much money do you receive for hour of work? \n"))
+work_hours = int(input("How many hours do you work in a month? \n"))
 
+#Calculating salary
+salary = money_hour * work_hours
+salary = float(salary)
+
+#Printing result
+print(f"Your total salary in a month is {salary}!")
